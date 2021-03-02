b1ba35742c874370e8e8102864f1b348dbce7f55
judge-seeds <testemail6092@gmail.com>
2021-03-02 08:33:22 +0530

    k

---
+++ script.js
@@ -0,0 +1,20 @@
+const date = new Date("Jan 5, 2022 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    var distance = date - now;
+  
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000); 
